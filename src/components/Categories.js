6b87b861a7af0cbc2a49d1e48a2d18424ea13023
Alier Philip <alierditmaguet@gmail.com>
2022-09-24 15:58:50 +0200

    Add Categories Component

---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categories = () => (
+  <div>
+    <button type="button" className="check">
+      Check Status
+    </button>
+  </div>
+);
+
+export default Categories;
