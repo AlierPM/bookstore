b771b4bc47a65b571a9903130d4013cf7ad976a3
Alier Philip <alierditmaguet@gmail.com>
2022-10-01 14:31:29 +0200

    Add reducers and actions to add and remove book

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,20 +1,37 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import RemoveBtn from './RemoveBtn';
+import { PropTypes } from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../store/books/books';
 
-const BookDetail = (props) => (
-  <div>
-    <p>
-      Book Title:
-      {props.title}
-    </p>
-    <p>
-      Author:
-      {props.author}
-    </p>
-    <RemoveBtn />
-  </div>
-);
+const BookDetail = (props) => {
+  const { id, title, author } = props;
+
+  const dispatch = useDispatch();
+  const handleRemove = () => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <div>
+      <p>
+        Book Title:
+        {title}
+      </p>
+      <p>
+        Author:
+        {author}
+      </p>
+      <button type="button" className="removeBtn" onClick={handleRemove}>
+        Remove
+      </button>
+    </div>
+  );
+};
+
+BookDetail.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
 
 export default BookDetail;
