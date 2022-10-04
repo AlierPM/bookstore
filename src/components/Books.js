16bc2d88753ad9fb2e2733ae93ce4d32efd9f587
Alier Philip <alierditmaguet@gmail.com>
2022-10-04 16:32:49 +0200

    Use useSelector to consume the state

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,29 +1,10 @@
-const Books = [
-  {
-    id: 1,
-    title: 'Money mastery from the Master ',
-    author: 'Gary Keesse',
-  },
-  {
-    id: 2,
-    title: 'Seven women and the secret of their Greatness',
-    author: 'Metaxas Eric',
-  },
-  {
-    id: 3,
-    title: 'Power of the Blood',
-    author: 'Mary K. Bartex',
-  },
-  {
-    id: 4,
-    title: 'Power of your mind',
-    author: 'Ps.Chris Oyahkilome',
-  },
-  {
-    id: 5,
-    title: 'Evidence for the Resurrrection of Jesus Christ',
-    author: 'Jose McDwell',
-  },
-];
+import Bookslist from './BooksList';
+import Diplaybooks from './Displaybooks';
 
+const Books = () => (
+  <>
+    <Bookslist />
+    <Diplaybooks />
+  </>
+);
 export default Books;
---
+++ src/components/Category.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Status } from '../Redux/categories/categories';
+
+function Author() {
+  const categories = useSelector((state) => state.categories.categories);
+  const dispatch = useDispatch();
+  useEffect(() => {}, [categories]);
+  return (
+    <div>
+      <button type="button" onClick={() => dispatch(Status())}>
+        Check status
+      </button>
+      {categories &&
+        categories.map((category) => <p key={Math.random()}>{category}</p>)}
+    </div>
+  );
+}
+
+export default Author;
